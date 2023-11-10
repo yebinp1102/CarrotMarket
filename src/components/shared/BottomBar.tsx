cfceea5c952502a0fabfa9c4820636f3964284f7
yebin Park <yebinp1102@naver.com>
2023-11-10 16:19:42 +0900

    Feat: add bottom menu bar

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,7 +1,32 @@
+import { Link, useLocation } from "react-router-dom"
+import { bottombarLinks } from "@/constants";
 
 const BottomBar = () => {
+  const {pathname} = useLocation();
+
   return (
-    <div>BottomBar</div>
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        
+        return (
+          <Link 
+            to={link.route} 
+            key={`bottombar-${link.label}`} 
+            className={`${isActive && "rounded-[10px] bg-[#f97516]"} flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img 
+              width={24}
+              height={24}
+              className={`${isActive && "invert-white"}`}
+              src={link.imgURL}
+              alt={link.label}
+            />
+            <p className="text-sm text-light-2">{link.label}</p>
+          </Link>
+        )
+      })}
+    </section>
   )
 }
 
